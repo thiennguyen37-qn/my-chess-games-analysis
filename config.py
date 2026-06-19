3cfdbe777e4912d2faa3cd3fab5c83f5a91e57e7
Nguyen Ho Bao Thien <baothien@cdtb.edu.vn>
2026-06-19 15:33:58 +0700

    Add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+USERNAME = "thien3703"
+
+HEADERS = {
+    "User-Agent": "research-script (contact: example@example.com)"
+}
+
+BASE_URL = "https://api.chess.com/pub/player"
+
+RAW_DATA_DIR  = "raw"
+RAW_DATA_PATH = f"{RAW_DATA_DIR}/{USERNAME}_rapid_games.csv"
